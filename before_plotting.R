53a7e9e88fef67db0ef33a69c3a9c6a7fc0ab05b
Kapya <kapsakala@gmail.com>
2015-08-10 01:23:44 +0200

    first steps and more comments added

--- before_plotting.R
+++
@@ -1,7 +0,0 @@
-#Reading in the dataset and extracting data for the first 2 days of February only
-household_power_consumption<-read.csv(~/R/household_power_consumption.txt, header=TRUE)
-#
-data<- household_power_consumption %>% mutate(Date=dmy(household_power_consumption$Date))
-      %>% filter(Date==dmy("01/02/2007")|Date==dmy("02/02/2007"))
-#Coercing the global active power from factor to numeric     
-      %>% mutate(Global_active_power=as.numeric(levels(data$Global_active_power))[data$Global_active_power])
--- plot1.R
+++ plot1.R
@@ -1,3 +1,12 @@
+#Reading in the dataset into R 
+household_power_consumption <- read.csv("~/R/household_power_consumption.txt", sep=";")
+
+#Extracting data for the first 2 days of February only and mutating the date column into POSTIXlt using lubridate
+data<- household_power_consumption %>% mutate(Date=dmy(household_power_consumption$Date))
+%>% filter(Date==dmy("01/02/2007")|Date==dmy("02/02/2007"))
+
 #Coercing the global active power from factor to numeric     
 data<- data %>% mutate(Global_active_power=as.numeric(levels(data$Global_active_power))[data$Global_active_power])
+
+#Plotting histogram of global active power
 hist(data$Global_active_power,xlab="Global active power (kilowatts)", col="red",main="Global active power")
